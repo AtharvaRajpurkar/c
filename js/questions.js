2e04a20b2ee5951811f619d7caa0a061cbc72dcf
AtharvaRajpurkar <75119919+AtharvaRajpurkar@users.noreply.github.com>
2022-03-20 19:37:35 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,66 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "What is the name of the Indian space agency?",
+    answer: "ISRO",
+    options: ["NASA", "Roscosmos", "JAXA", "ISRO"]
+  },
+  {
+    numb: 2,
+    question: "What is the male gamet cell known as?",
+  answer: "Sperm ",
+    options: ["Zygot", "Egg ", "Embro", "Sperm "]
+  },
+  {
+    numb: 3,
+    question: "What is the female gamet cell known as?",
+    answer: "Egg ",
+    options: ["Zygot", "Sperm", "Embro", "Egg "]
+  },
+  {
+    numb: 4,
+    question: "How many life process does a human have?",
+    answer: "7",
+    options: ["6", "3", "8", "7"]
+  },
+  {
+    numb: 5,
+    question: "How many groups are there in a periodic table?",
+    answer: "18",
+    options: ["15", "17", "8", "18"]
+  },
+  {
+    numb: 6,
+    question: "How many jouls in one kilojouls?",
+    answer: "1000",
+    options: ["1500", "500", "250","1000"]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+   {
+    numb: 7,
+    question: "Who was the first person to step on moon?",
+    answer: "Neil Armstrong",
+    options: ["Michael Collins", "Buzz Aldrin ", "Alan shepard","Neil Armstrong"]
+  },
+  {
+    numb: 8,
+    question: "Approximately how old is the Universe?",
+    answer: "13.8 billion years",
+    options: ["1.7 billion years", "16.9 billion years", "100 million years","13.8 billion years"]
+  },
+  {
+    numb: 8,
+    question: "Which instrument is used to measure voltage?",
+    answer: "Voltmeter",
+    options: ["Ammeter", "Resistor", "Cell", "Voltmeter"]
+  },
+  {
+    numb: 8,
+    question: "At what time of day a plant has the most starch?",
+    answer: "Night",
+    options: ["Night", "Mid day", "All the time","Day"]
+  }
+];
